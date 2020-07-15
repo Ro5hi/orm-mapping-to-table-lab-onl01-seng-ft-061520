6ffb26db61daec9fcefe70e048ab64468756a97b
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-15 17:23:45 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,30 @@
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id 
+  
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
+  
+  def initialize (name, grade, id=nil)
+  
+  def self.create_table 
+  
+  end 
+  
+  def self.drop_table 
+    
+  end 
+  
+  def sav
+    sql = <<-SQL 
+    
+    SQL
+  
+  end 
+  
+  def self.create 
+    
+  end 
   
 end
